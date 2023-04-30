168ad9ff640fb8325f60686a2eee25a1447c0ac3
Clément Sicard <c.clement.sicard@northvolt.com>
2023-04-30 13:21:06 +0200

    refactor(index.tsx): use react-router-dom to handle routing in the app and add ErrorPage component to handle 404 errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import "./styles.css";
-import Root from "./views/Root";
+import './styles.css'
+import Root from './views/Root'
+import ErrorPage from './views/ErrorPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />
+  }
+])
 
 ReactDOM.render(
   <React.StrictMode>
-    <Root />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-  document.getElementById("root"),
-);
+  document.getElementById('root')
+)
